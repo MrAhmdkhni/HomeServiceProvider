af77a2426ab665236e510dff112c853edb1b3944
Amirhosin <popularamirhosein@gmail.com>
2022-12-22 18:35:34 +0330

    complete MainServiceRepository interface

---
+++ src/main/java/ir/maktab/homeserviceprovider/repository/MainServiceRepository.java
@@ -0,0 +1,13 @@
+package ir.maktab.homeserviceprovider.repository;
+
+import ir.maktab.homeserviceprovider.entity.service.MainService;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MainServiceRepository extends JpaRepository<MainService, Long> {
+
+    Optional<MainService> findByName(String name);
+
+    int deleteByName (String name);
+}
