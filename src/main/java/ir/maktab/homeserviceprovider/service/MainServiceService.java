052f580f9737261bea802faa7006dea857f10fe8
Amirhosin <popularamirhosein@gmail.com>
2022-12-22 19:37:18 +0330

    complete MainServiceService interface and MainServiceServiceImpl class

---
+++ src/main/java/ir/maktab/homeserviceprovider/service/MainServiceService.java
@@ -0,0 +1,13 @@
+package ir.maktab.homeserviceprovider.service;
+
+import ir.maktab.homeserviceprovider.base.service.BaseService;
+import ir.maktab.homeserviceprovider.entity.service.MainService;
+
+import java.util.Optional;
+
+public interface MainServiceService extends BaseService<MainService, Long> {
+
+    Optional<MainService> findByName(String name);
+
+    int deleteByName (String name);
+}
---
+++ src/main/java/ir/maktab/homeserviceprovider/service/impl/MainServiceServiceImpl.java
@@ -0,0 +1,35 @@
+package ir.maktab.homeserviceprovider.service.impl;
+
+import ir.maktab.homeserviceprovider.base.service.impl.BaseServiceImpl;
+import ir.maktab.homeserviceprovider.entity.service.MainService;
+import ir.maktab.homeserviceprovider.repository.MainServiceRepository;
+import ir.maktab.homeserviceprovider.service.MainServiceService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class MainServiceServiceImpl
+        extends BaseServiceImpl<MainService, Long, MainServiceRepository>
+        implements MainServiceService {
+
+    public MainServiceServiceImpl(MainServiceRepository repository) {
+        super(repository);
+    }
+
+    @Override
+    public Optional<MainService> findByName(String name) {
+        try {
+            return repository.findByName(name);
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    @Transactional
+    public int deleteByName(String name) { //passed
+        return repository.deleteByName(name);
+    }
+}
