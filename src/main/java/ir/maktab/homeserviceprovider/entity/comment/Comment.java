44bb903c1efbb544548788e2a278a0cbc4c0b110
Amirhosin <popularamirhosein@gmail.com>
2022-12-29 23:15:16 +0330

    add @Builder in Comment class

--- src/main/java/ir/maktab/homeserviceprovider/entity/comment/Comment.java
+++ src/main/java/ir/maktab/homeserviceprovider/entity/comment/Comment.java
@@ -5,6 +5,7 @@ import ir.maktab.homeserviceprovider.entity.order.Order;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +14,7 @@ import lombok.Setter;
 @Setter
 @Getter
 @NoArgsConstructor
+@Builder
 public class Comment extends BaseEntity<Long> {
 
     private Integer score;
@@ -24,5 +26,11 @@ public class Comment extends BaseEntity<Long> {
     public Comment(Integer score, String comment) {
         this.score = score;
         this.comment = comment;
+    }
+
+    public Comment(Integer score, String comment, Order order) {
+        this.score = score;
+        this.comment = comment;
+        this.order = order;
     }
 }
