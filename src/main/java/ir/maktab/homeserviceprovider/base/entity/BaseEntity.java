8c329b3ed85bb08f51aa9fd2c34b3690da047b65
Amirhosin <popularamirhosein@gmail.com>
2022-12-29 23:11:50 +0330

    add equal and hashCode in BaseEntity

--- src/main/java/ir/maktab/homeserviceprovider/base/entity/BaseEntity.java
+++ src/main/java/ir/maktab/homeserviceprovider/base/entity/BaseEntity.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -23,5 +24,18 @@ public class BaseEntity<ID extends Serializable> implements Serializable{
 
     public boolean isNew() {
         return this.getId() == null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseEntity<?> that = (BaseEntity<?>) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
