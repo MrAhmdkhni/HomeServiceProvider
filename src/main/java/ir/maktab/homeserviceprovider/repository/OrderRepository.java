47f135fb99ec2eeba7aa6cd95d7c0bfaf3a97ec2
Amirhosin <popularamirhosein@gmail.com>
2023-01-12 23:12:59 +0330

    add findBySubServiceIdAndOrderStatus, calculateNumberOfRegisteredOrders and calculateNumberOfRegisteredOrders method in OrderRepository interface

--- src/main/java/ir/maktab/homeserviceprovider/repository/OrderRepository.java
+++ src/main/java/ir/maktab/homeserviceprovider/repository/OrderRepository.java
@@ -10,11 +10,10 @@ import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    @Query("from Order o where o.customer.id = :customerId")
+
     List<Order> findByCustomerId(Long customerId);
 
-    @Query("from Order o where o.customer.id = :customerId and o.orderStatus = :orderStatus")
-    List<Order> findByCustomerId(Long customerId, OrderStatus orderStatus);
+    List<Order> findByCustomerIdAndOrderStatus(Long customerId, OrderStatus orderStatus);
 
     @Modifying
     @Query("""
@@ -23,4 +22,23 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
             where o.id = :orderId and o.orderStatus = :orderStatus
             """)
     int changeOrderStatus(Long orderId, OrderStatus orderStatus, OrderStatus newOrderStatus);
+
+    @Query("""
+            from Order o
+            where o.subService.id = :subServiceId
+            and (o.orderStatus = :orderStatusOne or o.orderStatus = :orderStatusTwo)
+            """)
+    List<Order> findBySubServiceIdAndOrderStatus(Long subServiceId, OrderStatus orderStatusOne, OrderStatus orderStatusTwo);
+
+    @Query("""
+            select count(o.customer.id) from Order o
+            where o.customer.id = :customerId
+            """)
+    int calculateNumberOfRegisteredOrders(Long customerId);
+
+    @Query("""
+            select count(o.customer.id) from Order o
+            where o.customer.id = :customerId and o.orderStatus = :orderStatus
+            """)
+    int calculateNumberOfRegisteredOrders(Long customerId, OrderStatus orderStatus);
 }
