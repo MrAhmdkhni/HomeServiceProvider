84c6b0fafcc69974ae2e92da150f418a1ea78026
Amirhosin <popularamirhosein@gmail.com>
2023-01-12 23:52:41 +0330

    refactor HomeServiceProviderApplication

--- src/main/java/ir/maktab/homeserviceprovider/HomeServiceProviderApplication.java
+++ src/main/java/ir/maktab/homeserviceprovider/HomeServiceProviderApplication.java
@@ -1,30 +1,8 @@
 package ir.maktab.homeserviceprovider;
 
-import ir.maktab.homeserviceprovider.dto.ExpertFilterDTO;
-import ir.maktab.homeserviceprovider.entity.comment.Comment;
-import ir.maktab.homeserviceprovider.entity.offer.Offer;
-import ir.maktab.homeserviceprovider.entity.offer.TimeType;
-import ir.maktab.homeserviceprovider.entity.order.Order;
-import ir.maktab.homeserviceprovider.entity.order.OrderStatus;
-import ir.maktab.homeserviceprovider.entity.person.Customer;
-import ir.maktab.homeserviceprovider.entity.person.Expert;
-import ir.maktab.homeserviceprovider.entity.person.ExpertStatus;
-import ir.maktab.homeserviceprovider.entity.service.MainService;
-import ir.maktab.homeserviceprovider.entity.service.SubService;
-import ir.maktab.homeserviceprovider.service.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.Date;
-import java.util.Optional;
 
 @SpringBootApplication
 public class HomeServiceProviderApplication {
@@ -32,9 +10,4 @@ public class HomeServiceProviderApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(HomeServiceProviderApplication.class, args);
     }
-
-    @Bean
-    public RestTemplate restTemplate(){return new RestTemplate();}
-
-
 }
