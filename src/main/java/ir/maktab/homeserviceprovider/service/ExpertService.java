5e64f69ffbd6e9969315329a16467fdd9406a44f
Amirhosin <popularamirhosein@gmail.com>
2023-01-12 23:20:42 +0330

    fix and add some method in ExpertService interface and ExpertServiceImpl class

--- src/main/java/ir/maktab/homeserviceprovider/service/ExpertService.java
+++ src/main/java/ir/maktab/homeserviceprovider/service/ExpertService.java
@@ -3,6 +3,8 @@ package ir.maktab.homeserviceprovider.service;
 import ir.maktab.homeserviceprovider.base.service.BaseService;
 import ir.maktab.homeserviceprovider.dto.ExpertFilterDTO;
 import ir.maktab.homeserviceprovider.entity.offer.Offer;
+import ir.maktab.homeserviceprovider.entity.order.Order;
+import ir.maktab.homeserviceprovider.entity.order.OrderStatus;
 import ir.maktab.homeserviceprovider.entity.person.Expert;
 import ir.maktab.homeserviceprovider.entity.person.ExpertStatus;
 
@@ -13,13 +15,19 @@ public interface ExpertService extends BaseService<Expert, Long> {
 
     Optional<Expert> findByEmail(String email);
 
-    Optional<Expert> findByPhoneNumber(String phoneNumber);
+    Optional<Expert> findByUsername(String phoneNumber);
 
     int editPassword(Long expertId, String newPassword);
 
     int changeExpertStatus(Long expertId, ExpertStatus expertStatus);
 
     void signUp(Expert expert, String imageName, Long imageSize);
+
+    String signUpWithValidation(Expert expert, String imageName, Long imageSize);
+
+    int activeExpert(String email, ExpertStatus expertStatus);
+
+    List<Order> viewOrdersRelatedToExpert(Long expertId);
 
     void addOfferForOrder(Long expertId, Long orderId, Offer offer);//name must be change!
 
@@ -34,4 +42,14 @@ public interface ExpertService extends BaseService<Expert, Long> {
     void updateCredit(Long expertId, Long newCredit);
 
     List<Expert> expertsFilter(ExpertFilterDTO expertDTO);
+
+    List<Offer> viewOfferHistory(Long expertId, boolean isAccept);
+
+    List<Order> viewOrderHistory(Long expertId, boolean isAccept);
+
+    List<Order> viewOrderHistory(Long expertId, boolean isAccept, OrderStatus orderStatus);
+
+    Long viewCredit(Long expertId);
+
+
 }
--- src/main/java/ir/maktab/homeserviceprovider/service/impl/ExpertServiceImpl.java
+++ src/main/java/ir/maktab/homeserviceprovider/service/impl/ExpertServiceImpl.java
@@ -2,12 +2,12 @@ package ir.maktab.homeserviceprovider.service.impl;
 
 import ir.maktab.homeserviceprovider.base.service.impl.BaseServiceImpl;
 import ir.maktab.homeserviceprovider.dto.ExpertFilterDTO;
+import ir.maktab.homeserviceprovider.entity.token.ConfirmationToken;
 import ir.maktab.homeserviceprovider.entity.comment.Comment;
 import ir.maktab.homeserviceprovider.entity.offer.Offer;
 import ir.maktab.homeserviceprovider.entity.order.Order;
 import ir.maktab.homeserviceprovider.entity.order.OrderStatus;
-import ir.maktab.homeserviceprovider.entity.person.Expert;
-import ir.maktab.homeserviceprovider.entity.person.ExpertStatus;
+import ir.maktab.homeserviceprovider.entity.person.*;
 import ir.maktab.homeserviceprovider.exception.*;
 import ir.maktab.homeserviceprovider.repository.ExpertRepository;
 import ir.maktab.homeserviceprovider.service.*;
@@ -18,12 +18,15 @@ import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class ExpertServiceImpl
@@ -32,16 +35,19 @@ public class ExpertServiceImpl
     private final OfferService offerService;
     private final OrderService orderService;
     private final CommentService commentService;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final ConfirmationTokenService confirmationTokenService;
     @PersistenceContext
     private EntityManager entityManager;
 
-    public ExpertServiceImpl(ExpertRepository repository, OfferService offerService, OrderService orderService, CommentService commentService) {
+    public ExpertServiceImpl(ExpertRepository repository, OfferService offerService, OrderService orderService, CommentService commentService, BCryptPasswordEncoder passwordEncoder, ConfirmationTokenService confirmationTokenService) {
         super(repository);
         this.offerService = offerService;
         this.orderService = orderService;
         this.commentService = commentService;
+        this.passwordEncoder = passwordEncoder;
+        this.confirmationTokenService = confirmationTokenService;
     }
-
 
 
     @Override
@@ -54,9 +60,9 @@ public class ExpertServiceImpl
     }
 
     @Override
-    public Optional<Expert> findByPhoneNumber(String phoneNumber) {
+    public Optional<Expert> findByUsername(String username) {
         try {
-            return repository.findByPhoneNumber(phoneNumber);
+            return repository.findByUsername(username);
         } catch (Exception e) {
             return Optional.empty();
         }
@@ -67,8 +73,7 @@ public class ExpertServiceImpl
     public int editPassword(Long expertId, String newPassword) {
         if (findById(expertId).isEmpty())
             throw new ExpertNotFoundException("this expert does not exist!");
-        Validation.checkPassword(newPassword);
-        return repository.editPassword(expertId, newPassword);
+        return repository.editPassword(expertId, passwordEncoder.encode(newPassword));
     }
 
     @Override
@@ -79,20 +84,77 @@ public class ExpertServiceImpl
 
     @Override
     public void signUp(Expert expert, String imageName, Long imageSize) {
-        if (findByPhoneNumber(expert.getPhoneNumber()).isPresent())
-            throw new DuplicatePhoneNumberException("this phone number already exist!");
-        else if (findByEmail(expert.getEmail()).isPresent())
-            throw new DuplicateEmailException("this email number already exist!");
-        Validation.checkPhoneNumber(expert.getPhoneNumber());
-        Validation.checkEmail(expert.getEmail());
-        Validation.checkPassword(expert.getPassword());
+        if (findByEmail(expert.getEmail()).isPresent())
+            throw new DuplicateEmailException("this email already exist!");
+        if (findByUsername(expert.getUsername()).isPresent())
+            throw new DuplicateUsernameException("this username already exist!");
         Validation.checkImage(imageName, imageSize);
 
-        expert.setScore(0);
-        expert.setCredit(0L);
-        expert.setIsActive(true);
+        expert.setPassword(passwordEncoder.encode(expert.getPassword()));
         expert.setExpertStatus(ExpertStatus.NEW);
+        expert.setRole(Role.ROLE_EXPERT);
+        expert.setIsActive(false);
+        expert.setCredit(0L);
+        expert.setScore(0);
         saveOrUpdate(expert);
+    }
+
+    @Override
+    public String signUpWithValidation(Expert expert, String imageName, Long imageSize) {
+        if (findByEmail(expert.getEmail()).isPresent()) {
+            Expert loadedExpert = findByEmail(expert.getEmail()).get();
+
+            if (!loadedExpert.getIsActive()) {
+                String token = UUID.randomUUID().toString();
+                saveConfirmationToken(loadedExpert, token);
+                return token;
+            }
+            throw new DuplicateEmailException("this email already exist!");
+        }
+        if (findByUsername(expert.getUsername()).isPresent())
+            throw new DuplicateUsernameException("this username already exist!");
+        Validation.checkImage(imageName, imageSize);
+
+        expert.setPassword(passwordEncoder.encode(expert.getPassword()));
+        expert.setExpertStatus(ExpertStatus.NEW);
+        expert.setRole(Role.ROLE_EXPERT);
+        expert.setIsActive(false);
+        expert.setCredit(0L);
+        expert.setScore(0);
+        saveOrUpdate(expert);
+
+        String token = UUID.randomUUID().toString();
+        saveConfirmationToken(expert, token);
+        return token;
+    }
+
+    private void saveConfirmationToken(Person person, String token) {
+        ConfirmationToken confirmationToken = new ConfirmationToken(
+                token, LocalDateTime.now(), LocalDateTime.now().plusMinutes(15), person
+        );
+        confirmationTokenService.saveConfirmationToken(confirmationToken);
+    }
+
+    @Override
+    public int activeExpert(String email, ExpertStatus expertStatus) {
+        return repository.activeExpert(email, expertStatus);
+    }
+
+    @Override
+    public List<Order> viewOrdersRelatedToExpert(Long expertId) {
+        List<Order> orders = new ArrayList<>();
+        Optional<Expert> expert = findById(expertId);
+        if (expert.isEmpty())
+            throw new ExpertNotFoundException("this expert does not exist!");
+        expert.get().getSubServices().forEach(subService -> {
+            List<Order> filteredOrder = orderService
+                    .findBySubServiceIdAndOrderStatus(
+                            subService.getId(),
+                            OrderStatus.WAITING_FOR_EXPERT_SELECTION,
+                            OrderStatus.WAITING_FOR_EXPERT_SUGGESTION);
+            orders.addAll(filteredOrder);
+        });
+        return orders;
     }
 
     @Override
@@ -177,27 +239,28 @@ public class ExpertServiceImpl
             String lastname = "%" + expertDTO.getLastname() + "%";
             predicateList.add(criteriaBuilder.like(expertRoot.get("lastname"), lastname));
         }
-        if (expertDTO.getPhoneNumber() != null) {
-            String phoneNumber = "%" + expertDTO.getPhoneNumber() + "%";
-            predicateList.add(criteriaBuilder.like(expertRoot.get("phoneNumber"), phoneNumber));
-        }
         if (expertDTO.getEmail() != null) {
             String email = "%" + expertDTO.getEmail() + "%";
             predicateList.add(criteriaBuilder.like(expertRoot.get("email"), email));
         }
-        if (expertDTO.getExpertStatus() != null) {
-            String expertStatus = "%" + expertDTO.getExpertStatus()+ "%";
-            predicateList.add(criteriaBuilder.like(expertRoot.get("expertStatus"), expertStatus));
+        if (expertDTO.getUsername() != null) {
+            String username = "%" + expertDTO.getUsername() + "%";
+            predicateList.add(criteriaBuilder.like(expertRoot.get("username"), username));
         }
         if (expertDTO.getIsActive() != null) {
-            String isActive = "%" + expertDTO.getIsActive() + "%";
-            predicateList.add(criteriaBuilder.like(expertRoot.get("isActive"), isActive));
+            predicateList.add(criteriaBuilder.equal(expertRoot.get("isActive"), expertDTO.getIsActive()));
         }
+        if (expertDTO.getExpertStatus() != null) {
+            predicateList.add(criteriaBuilder.equal(expertRoot.get("expertStatus"), expertDTO.getExpertStatus()));
+        }
+
         if (expertDTO.getMinScore() == null && expertDTO.getMaxScore() != null) {
             predicateList.add(criteriaBuilder.lt(expertRoot.get("score"), expertDTO.getMaxScore()));
-        } else if (expertDTO.getMinScore() != null && expertDTO.getMaxScore() == null) {
+        }
+        if (expertDTO.getMinScore() != null && expertDTO.getMaxScore() == null) {
             predicateList.add(criteriaBuilder.gt(expertRoot.get("score"), expertDTO.getMinScore()));
-        } else if (expertDTO.getMinScore() != null && expertDTO.getMaxScore() != null) {
+        }
+        if (expertDTO.getMinScore() != null && expertDTO.getMaxScore() != null) {
             predicateList.add(criteriaBuilder.between(expertRoot.get("score"), expertDTO.getMinScore(), expertDTO.getMaxScore()));
         }
 
@@ -210,5 +273,51 @@ public class ExpertServiceImpl
         if (expertDTO.getMinCredit() != null && expertDTO.getMaxCredit() != null) {
             predicateList.add(criteriaBuilder.between(expertRoot.get("credit"), expertDTO.getMinCredit(), expertDTO.getMaxCredit()));
         }
+
+        if (expertDTO.getMinCreationDate() != null && expertDTO.getMaxCreationDate() != null) {
+            predicateList
+                    .add(criteriaBuilder
+                            .between(expertRoot.get("creationDate"),
+                                    LocalDateTime.parse(expertDTO.getMinCreationDate()),
+                                    LocalDateTime.parse(expertDTO.getMaxCreationDate())));
+        }
     }
+
+    @Override
+    public List<Offer> viewOfferHistory(Long expertId, boolean isAccept) {
+        Optional<Expert> expert = findById(expertId);
+        if (expert.isEmpty())
+            throw new ExpertNotFoundException("this expert does not exist!");
+        return offerService.findOfferByExpertIdAndIsAccept(expertId, isAccept);
+    }
+
+    @Override
+    public List<Order> viewOrderHistory(Long expertId, boolean isAccept) {
+        List<Order> orders = new ArrayList<>();
+        List<Offer> expertOffers = viewOfferHistory(expertId, isAccept);
+        expertOffers.forEach(offer ->
+                orders.add(offer.getOrder()));
+        return orders;
+    }
+
+    @Override
+    public List<Order> viewOrderHistory(Long expertId, boolean isAccept, OrderStatus orderStatus) {
+        List<Order> orders = new ArrayList<>();
+        List<Offer> expertOffers = viewOfferHistory(expertId, isAccept);
+        expertOffers.stream()
+                .filter(offer -> offer.getOrder().getOrderStatus().equals(orderStatus))
+                .forEach(offer -> {
+                    orders.add(offer.getOrder());
+                });
+        return orders;
+    }
+
+    @Override
+    public Long viewCredit(Long expertId) {
+        Optional<Expert> expert = findById(expertId);
+        if (expert.isEmpty())
+            throw new ExpertNotFoundException("this expert does not exist!");
+        return expert.get().getCredit();
+    }
+
 }
