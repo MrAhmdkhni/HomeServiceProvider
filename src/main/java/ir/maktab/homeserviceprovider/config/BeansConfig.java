dc4d344073d8db5a64f54274334e57135114d1c5
Amirhosin <popularamirhosein@gmail.com>
2023-01-12 23:35:38 +0330

    add and complete BeanConfig class

---
+++ src/main/java/ir/maktab/homeserviceprovider/config/BeansConfig.java
@@ -0,0 +1,24 @@
+package ir.maktab.homeserviceprovider.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class BeansConfig {
+
+    @Bean
+    public RestTemplate restTemplate(){return new RestTemplate();}
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
