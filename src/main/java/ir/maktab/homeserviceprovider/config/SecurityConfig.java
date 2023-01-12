7a888c260f53784443e991891504220499d053f9
Amirhosin <popularamirhosein@gmail.com>
2023-01-12 23:35:07 +0330

    add and complete SecurityConfig class

---
+++ src/main/java/ir/maktab/homeserviceprovider/config/SecurityConfig.java
@@ -0,0 +1,62 @@
+package ir.maktab.homeserviceprovider.config;
+
+import ir.maktab.homeserviceprovider.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+    private final PersonService personService;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public SecurityConfig(PersonService personService, BCryptPasswordEncoder passwordEncoder) {
+        this.personService = personService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests().requestMatchers("/signup/**").permitAll()
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .requestMatchers("/expert/**").hasRole("EXPERT")
+                .requestMatchers("/customer/**").hasRole("CUSTOMER")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(
+                        username -> personService
+                                .findByUsername(username)
+                                .orElseThrow(
+                                        () -> new UsernameNotFoundException(
+                                                String.format("there is no user with this %s!", username))))
+                .passwordEncoder(passwordEncoder);
+    }
+}
