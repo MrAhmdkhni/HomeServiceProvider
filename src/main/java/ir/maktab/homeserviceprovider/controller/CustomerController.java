d64266bfd8c20f88e6d2943d9c0ed95f544f02bd
Amirhosin <popularamirhosein@gmail.com>
2023-01-12 23:30:42 +0330

    fix and complete CustomerController class

--- src/main/java/ir/maktab/homeserviceprovider/controller/CustomerController.java
+++ src/main/java/ir/maktab/homeserviceprovider/controller/CustomerController.java
@@ -6,7 +6,10 @@ import ir.maktab.homeserviceprovider.entity.order.Order;
 import ir.maktab.homeserviceprovider.entity.person.Customer;
 import ir.maktab.homeserviceprovider.service.CustomerService;
 import ir.maktab.homeserviceprovider.util.ReCaptchaResponse;
+import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -23,35 +26,27 @@ public class CustomerController {
 
     private final CustomerService customerService;
     private final RestTemplate restTemplate;
+    private final ModelMapper mapper;
 
-    public CustomerController(CustomerService customerService, RestTemplate restTemplate) {
+    public CustomerController(CustomerService customerService, RestTemplate restTemplate, ModelMapper mapper) {
         this.customerService = customerService;
         this.restTemplate = restTemplate;
+        this.mapper = mapper;
     }
 
 
     @GetMapping("/findByEmail/{email}")
     @ResponseBody
-    public PersonDTO findByEmail(@PathVariable String email){
+    public PersonDTO findByEmail(@PathVariable String email) {
         Customer customer = customerService.findByEmail(email).get();
-         return new PersonDTO(
-                 customer.getFirstname(),
-                 customer.getLastname(),
-                 customer.getPhoneNumber(),
-                 customer.getEmail(),
-                 customer.getPassword());
+        return mapper.map(customer, PersonDTO.class);
     }
 
-    @GetMapping("/findByPhoneNumber/{phoneNumber}")
+    @GetMapping("/findByUsername/{username}")
     @ResponseBody
-    public PersonDTO findByPhoneNumber(@PathVariable String phoneNumber) {
-        Customer customer = customerService.findByPhoneNumber(phoneNumber).get();
-        return new PersonDTO(
-                customer.getFirstname(),
-                customer.getLastname(),
-                customer.getPhoneNumber(),
-                customer.getEmail(),
-                customer.getPassword());
+    public PersonDTO findByUsername(@PathVariable String username) {
+        Customer customer = customerService.findByUsername(username).get();
+        return mapper.map(customer, PersonDTO.class);
     }
 
     @GetMapping("/existsByEmail/{email}")
@@ -62,30 +57,32 @@ public class CustomerController {
 
     @PostMapping("/signup")
     @ResponseBody
-    public void singUp(@RequestBody PersonDTO personDTO) {
+    public void singUp(@Valid @RequestBody PersonDTO personDTO) {
         customerService.signUp(
                 Customer.builder()
                         .firstname(personDTO.getFirstname())
                         .lastname(personDTO.getLastname())
-                        .phoneNumber(personDTO.getPhoneNumber())
                         .email(personDTO.getEmail())
+                        .username(personDTO.getUsername())
                         .password(personDTO.getPassword())
-                        .build());
+                        .build()
+        );
     }
 
-    @PutMapping("/editPassword")
+    @PostMapping("/editPassword")
     @ResponseBody
-    public int editPassword(@RequestBody PasswordDTO passwordDTO) {
-        return customerService.editPassword(passwordDTO.getId(), passwordDTO.getPassword());
+    public int editPassword(@Valid @RequestBody PasswordDTO passwordDTO, Authentication authentication) {
+        Customer authenticatedCustomer = (Customer) authentication.getPrincipal();
+        return customerService.editPassword(authenticatedCustomer.getId(), passwordDTO.getPassword());
     }
 
     @GetMapping("/findAllMainService")
     @ResponseBody
     public List<MainServiceDTO> findAllMainService() {
         List<MainServiceDTO> mainServiceDTOS = new ArrayList<>();
-        customerService.findAllMainService().forEach(mainService -> {
-            mainServiceDTOS.add(new MainServiceDTO(mainService.getName()));
-        });
+        customerService.findAllMainService().forEach(mainService ->
+                mainServiceDTOS.add(mapper.map(mainService, MainServiceDTO.class))
+        );
         return mainServiceDTOS;
     }
 
@@ -93,26 +90,24 @@ public class CustomerController {
     @ResponseBody
     public List<SubServiceDTO> findAllSubService() {
         List<SubServiceDTO> subServiceDTOS = new ArrayList<>();
-        customerService.findAllSubService().forEach(subService -> {
-            subServiceDTOS.add(new SubServiceDTO(
-                    subService.getName(),
-                    subService.getBasePrice(),
-                    subService.getDescription(),
-                    subService.getMainService().getName()));
-        });
+        customerService.findAllSubService().forEach(subService ->
+                subServiceDTOS.add(mapper.map(subService, SubServiceDTO.class))
+        );
         return subServiceDTOS;
     }
 
     @PostMapping("/addOrder")
     @ResponseBody
-    public void addOrder(@RequestBody CustomerSubServiceOrderDTO customerSubServiceOrderDTO) {
-        customerService.addOrder(customerSubServiceOrderDTO.getCustomerId(), customerSubServiceOrderDTO.getSubServiceId(),
-                Order.builder().proposedPrice(customerSubServiceOrderDTO.getProposedPrice())
-                        .description(customerSubServiceOrderDTO.getDescription())
+    public void addOrder(@RequestBody SubServiceOrderDTO subServiceOrderDTO, Authentication authentication) {
+        Customer authenticatedCustomer = (Customer) authentication.getPrincipal();
+        customerService.addOrder(authenticatedCustomer.getId(), subServiceOrderDTO.getSubServiceId(),
+                Order.builder()
+                        .proposedPrice(subServiceOrderDTO.getProposedPrice())
+                        .description(subServiceOrderDTO.getDescription())
                         .executionTime(LocalDateTime.of(
-                                        LocalDate.parse(customerSubServiceOrderDTO.getExecutionDate()),
-                                        LocalTime.parse(customerSubServiceOrderDTO.getExecutionTime())))
-                        .address(customerSubServiceOrderDTO.getAddress())
+                                LocalDate.parse(subServiceOrderDTO.getExecutionDate()),
+                                LocalTime.parse(subServiceOrderDTO.getExecutionTime())))
+                        .address(subServiceOrderDTO.getAddress())
                         .build());
     }
 
@@ -124,21 +119,6 @@ public class CustomerController {
                         .score(orderExpertCommentDTO.getScore())
                         .comment(orderExpertCommentDTO.getComment())
                         .build());
-    }
-
-    @GetMapping("/viewCustomerOrders/{customerId}")
-    @ResponseBody
-    public List<OrderDTO> viewCustomerOrders(@PathVariable Long customerId){
-        List<OrderDTO> orderDTOS = new ArrayList<>();
-        customerService.viewCustomerOrders(customerId).forEach(order -> {
-            orderDTOS.add(new OrderDTO(
-                    order.getCreationDate(),
-                    order.getProposedPrice(),
-                    order.getDescription(),
-                    order.getSubService().getName(),
-                    order.getOrderStatus()));
-        });
-        return orderDTOS;
     }
 
     @GetMapping("/viewOffersBaseOnProposedPrice/{orderId}")
@@ -187,10 +167,11 @@ public class CustomerController {
         return customerService.changeOrderStatusAfterStarted(orderId);
     }
 
-    @PutMapping("/payFromCredit/{orderId}/{customerId}/{expertId}/{amount}")
+    @PutMapping("/payFromCredit/{orderId}/{expertId}/{amount}")
     @ResponseBody
-    public void payFromCredit(@PathVariable Long orderId, @PathVariable Long customerId, @PathVariable Long expertId, @PathVariable Long amount) {
-        customerService.payFromCredit(orderId, customerId, expertId, amount);
+    public void payFromCredit(@PathVariable Long orderId, @PathVariable Long expertId, @PathVariable Long amount, Authentication authentication) {
+        Customer authenticatedCustomer = (Customer) authentication.getPrincipal();
+        customerService.payFromCredit(orderId, authenticatedCustomer.getId(), expertId, amount);
     }
 
     @GetMapping("/onlinePay")
@@ -203,10 +184,42 @@ public class CustomerController {
     private String pay(@RequestParam("g-recaptcha-response") String captcha) {
         String url = "https://www.google.com/recaptcha/api/siteverify";
         String params = "?secret=6LcKj7gjAAAAAAYY0U0irX6rLHFUuETgBmxZtjO1&response=" + captcha;
-        ReCaptchaResponse reCaptchaResponse = restTemplate.exchange(url + params, HttpMethod.POST, null,ReCaptchaResponse.class).getBody();
-        if (reCaptchaResponse.isSuccess()){
+        ReCaptchaResponse reCaptchaResponse = restTemplate.exchange(url + params, HttpMethod.POST, null, ReCaptchaResponse.class).getBody();
+        if (reCaptchaResponse.isSuccess()) {
             return "done";
         }
         return "invalid";
+    }
+
+    @GetMapping("/viewOrderHistory")
+    @ResponseBody
+    public List<OrderDTO> viewOrderHistory(Authentication authentication) {
+        Customer authenticatedCustomer = (Customer) authentication.getPrincipal();
+        List<OrderDTO> orderDTOS = new ArrayList<>();
+        customerService.viewOrderHistory(authenticatedCustomer.getId())
+                .forEach(
+                        order -> orderDTOS.add(
+                                mapper.map(order, OrderDTO.class))
+        );
+        return orderDTOS;
+    }
+
+    @PostMapping("/viewOrderHistory")
+    @ResponseBody
+    public List<OrderDTO> viewOrderHistory(@RequestBody CustomerOrderStatusDTO customerOrderStatusDTO, Authentication authentication) {
+        Customer authenticatedCustomer = (Customer) authentication.getPrincipal();
+        List<OrderDTO> orderDTOS = new ArrayList<>();
+        customerService
+                .viewOrderHistory(
+                        authenticatedCustomer.getId(),
+                        customerOrderStatusDTO.getOrderStatus())
+                .forEach(order -> orderDTOS.add(mapper.map(order, OrderDTO.class)));
+        return orderDTOS;
+    }
+
+    @GetMapping("/viewCredit")
+    public Long viewCredit(Authentication authentication) {
+        Customer authenticatedCustomer = (Customer) authentication.getPrincipal();
+        return customerService.viewCredit(authenticatedCustomer.getId());
     }
 }
