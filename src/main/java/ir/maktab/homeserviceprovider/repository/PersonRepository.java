d126691f70190cc8b8809f6ee2fe245994e5f6b1
Amirhosin <popularamirhosein@gmail.com>
2023-01-16 16:08:01 +0330

    Delete some comments

--- src/main/java/ir/maktab/homeserviceprovider/repository/PersonRepository.java
+++ src/main/java/ir/maktab/homeserviceprovider/repository/PersonRepository.java
@@ -2,18 +2,10 @@ package ir.maktab.homeserviceprovider.repository;
 
 import ir.maktab.homeserviceprovider.entity.person.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
     Optional<Person> findByUsername(String username);
-
-    @Transactional
-    @Modifying
-    @Query("update Person p set p.isActive = true where p.email=?1")
-    int activePerson(String email);
 }
