3ca0ba465cdf321e382a06950ae419ea1669bdd4
Amirhosin <popularamirhosein@gmail.com>
2023-01-19 00:09:31 +0330

    delete and rename a method in ConfirmationTokenRepository

--- src/main/java/ir/maktab/homeserviceprovider/repository/ConfirmationTokenRepository.java
+++ src/main/java/ir/maktab/homeserviceprovider/repository/ConfirmationTokenRepository.java
@@ -2,19 +2,10 @@ package ir.maktab.homeserviceprovider.repository;
 
 import ir.maktab.homeserviceprovider.entity.token.ConfirmationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
 
-    Optional<ConfirmationToken> findByToken(String token);
-
-    @Transactional
-    @Modifying
-    @Query("update ConfirmationToken ct set ct.confirmedAt = ?2 where ct.token = ?1")
-    int updateConfirmedAt(String token, LocalDateTime localDateTime);
+    Optional<ConfirmationToken> findByConfirmationToken(String confirmationToken);
 }
