1d81295b84727bbe89b96cdef51d96bd24f1d6b9
Amirhosin <popularamirhosein@gmail.com>
2023-01-19 00:14:45 +0330

    refactor PersonService interface and PersonServiceImpl class

--- src/main/java/ir/maktab/homeserviceprovider/service/PersonService.java
+++ src/main/java/ir/maktab/homeserviceprovider/service/PersonService.java
@@ -1,10 +1,11 @@
 package ir.maktab.homeserviceprovider.service;
 
+import ir.maktab.homeserviceprovider.base.service.BaseService;
 import ir.maktab.homeserviceprovider.entity.person.Person;
 
 import java.util.Optional;
 
-public interface PersonService {
+public interface PersonService extends BaseService<Person, Long> {
 
     Optional<Person> findByUsername(String username);
 }
--- src/main/java/ir/maktab/homeserviceprovider/service/impl/PersonServiceImpl.java
+++ src/main/java/ir/maktab/homeserviceprovider/service/impl/PersonServiceImpl.java
@@ -1,38 +1,29 @@
 package ir.maktab.homeserviceprovider.service.impl;
 
+import ir.maktab.homeserviceprovider.base.service.impl.BaseServiceImpl;
 import ir.maktab.homeserviceprovider.entity.person.Person;
 import ir.maktab.homeserviceprovider.repository.PersonRepository;
 import ir.maktab.homeserviceprovider.service.PersonService;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
-public class PersonServiceImpl implements PersonService, UserDetailsService {
+public class PersonServiceImpl
+        extends BaseServiceImpl<Person, Long, PersonRepository>
+        implements PersonService {
 
-    private final PersonRepository personRepository;
-
-    public PersonServiceImpl(PersonRepository personRepository) {
-        this.personRepository = personRepository;
+    public PersonServiceImpl(PersonRepository repository) {
+        super(repository);
     }
 
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return personRepository
-                .findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException(String.format("User with username %s not found", username)));
-    }
 
     @Override
     @Transactional
     public Optional<Person> findByUsername(String username) {
         try {
-            return personRepository.findByUsername(username);
+            return repository.findByUsername(username);
         } catch (Exception e) {
             return Optional.empty();
         }
